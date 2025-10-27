c91234faeb54ac24a2eae9d76274846d581e8fc6
Ian Masinde <hymasindeian@gmail.com>
2025-10-27 13:07:12 +0300

    changes

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,12 +1,4 @@
 const serverless = require('serverless-http');
 const app = require('../app');
 
-// Export the app for local development
-if (require.main === module) {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-}
-
 module.exports = serverless(app);
