903e5662e4e05954fe73bfa8945bfafb6b2c5ae0
Ian Masinde <hymasindeian@gmail.com>
2025-10-27 15:35:45 +0300

    changes

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,4 +1,7 @@
 const serverless = require('serverless-http');
 const app = require('../app');
 
+// Set NODE_ENV for production if not set
+process.env.NODE_ENV = process.env.NODE_ENV || 'production';
+
 module.exports = serverless(app);
