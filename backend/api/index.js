1561033c4d9793805d9c264cf1d0c5e1abd3f9cd
Ian Masinde <hymasindeian@gmail.com>
2025-10-27 15:31:49 +0300

    change

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,10 +1,4 @@
 const serverless = require('serverless-http');
 const app = require('../app');
 
-// Only wrap with serverless when running on Vercel
-if (process.env.VERCEL) {
-  module.exports = serverless(app);
-} else {
-  // For local development, export the app directly
-  module.exports = app;
-}
+module.exports = serverless(app);
