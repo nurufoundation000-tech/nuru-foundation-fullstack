0a45ccaab21f6ae08253cac52ecd548be7367393
Ian Masinde <hymasindeian@gmail.com>
2025-10-28 13:11:29 +0300

    C

--- frontend/scripts/auth.js
+++ frontend/scripts/auth.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = '/api';
+const API_BASE_URL = 'localhost' ? 'http://localhost:5000/api'  : 'https://nuru-foundation-fullstack.vercel.app/api';
 
 class AuthService {
     constructor() {
