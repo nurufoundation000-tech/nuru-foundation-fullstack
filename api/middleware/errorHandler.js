176ab601e83d6444ecab10cd321ff1ff3ce2057f
Ian Masinde <hymasindeian@gmail.com>
2025-11-20 11:57:21 +0300

    deleted everything starting over

--- api/middleware/errorHandler.js
+++
@@ -1,7 +0,0 @@
-// api/middleware/errorHandler.js
-const errorHandler = (error, req, res, next) => {
-  console.error('Error:', error);
-  res.status(500).json({ message: 'Something went wrong!' });
-};
-
-module.exports = errorHandler;
